7a3ab415dd08dc9bb7d15b5d01d167b03990ea5f
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-08-14 15:40:00 -0500

    feat: Use optional in two new JPA Methods

--- src/main/java/com/jpaappj3/jpa_app/JpaAppApplication.java
+++ src/main/java/com/jpaappj3/jpa_app/JpaAppApplication.java
@@ -2,6 +2,7 @@ package com.jpaappj3.jpa_app;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -29,8 +30,15 @@ public class JpaAppApplication implements CommandLineRunner{
 		// List<Person> persons = (List<Person>) personRepository.findByProgrammingLanguage("java");
 		// persons.stream().forEach(person -> System.out.println(person));
 		// method 2: search by programming languange and name
-		List<Person> persons = (List<Person>) personRepository.findByProgrammingLanguage("java","os");
-		persons.stream().forEach(person -> System.out.println("finded: "+ person));
+		// List<Person> persons = (List<Person>) personRepository.findByProgrammingLanguageAndName("java","Johlver");
+		// persons.stream().forEach(person -> System.out.println("finded: "+ person));
+		// method 3: obtain person data
+		// List<Object[]> personValues = personRepository.obtenerPersonData();
+		// personValues.stream().forEach(person -> System.out.println("person: " + person[0]));
+		// method 4: obtain a person by name using optional
+		Optional<Person> optionalPerson = personRepository.findOneName("german");
+		String rta = optionalPerson.isPresent() ? optionalPerson.orElseThrow().toString() : "Not found";
+		System.out.println("1: " + rta);
 	}
 
 	@Transactional
--- src/main/java/com/jpaappj3/jpa_app/repositories/PersonRepository.java
+++ src/main/java/com/jpaappj3/jpa_app/repositories/PersonRepository.java
@@ -1,15 +1,24 @@
 package com.jpaappj3.jpa_app.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.jpaappj3.jpa_app.entities.Person;
 
+
 public interface PersonRepository extends JpaRepository<Person,Long> {
     List<Person> findByProgrammingLanguage(String programmingLanguage);
 
-    @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2")
-    List<Person> findByProgrammingLanguage(String programmingLanguage,String name);
+    // @Query("select p from Person p where p.programmingLanguage=?1 and p.name=?2")
+    // List<Person> findByProgrammingLanguage(String programmingLanguage,String name);
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
+
+    @Query("select p.name, p.programmingLanguage from Person p")
+    List<Object[]> obtenerPersonData();
+    
+    @Query("select p from Person p where p.name=?1")
+    Optional<Person> findOneName(String name);
 }
